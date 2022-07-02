5803496d69e7daa4d4d6771d341631b9193b45ff
Tomasz Dydacki <b4shiken@hotmail.com>
2022-07-02 21:38:29 +0200

    5

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,11 @@
+help()
+{
+echo "Lista dostępnych opcji: "
+echo "--date #wyświetli dzisiejszą datę"
+echo "--logs utworzy automatycznie 100 plików logx.txt x-nrpliku od 1 do100 plik zawiera nazwę, nazwę skryptu, datę"
+echo "--logs a utworzy automatycznie a plików logx.txt x-nr pliku od 1 do 100 plik zawiera nazwę, nazwę skryptu, datę"
+echo "--help wyświetli listę dostępnych opcji"
+}
 while [ "$1" != "" ];
 do
 	case $1 in
@@ -9,7 +17,7 @@ do
 			if [ $# -eq 2 ];
 			then
 				shift
-				for i in {1..'$z'}; 
+				for i in {1..$z}; 
 				do
 					echo "log$i.txt">>log$i.txt
 					echo "skrypt.sh">>log$i.txt
