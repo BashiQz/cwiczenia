a633b643b4d81468b3999f5f632d9dffce6a6f03
Tomasz Dydacki <b4shiken@hotmail.com>
2022-07-02 21:45:26 +0200

    6

--- skrypt.sh
+++ skrypt.sh
@@ -1,18 +1,18 @@
 help()
 {
 echo "Lista dostępnych opcji: "
-echo "--date #wyświetli dzisiejszą datę"
-echo "--logs utworzy automatycznie 100 plików logx.txt x-nrpliku od 1 do100 plik zawiera nazwę, nazwę skryptu, datę"
-echo "--logs a utworzy automatycznie a plików logx.txt x-nr pliku od 1 do 100 plik zawiera nazwę, nazwę skryptu, datę"
-echo "--help wyświetli listę dostępnych opcji"
+echo "--date / -d #wyświetli dzisiejszą datę"
+echo "--logs / -l utworzy automatycznie 100 plików logx.txt x-nrpliku od 1 do100 plik zawiera nazwę, nazwę skryptu, datę"
+echo "--logs a / -l a utworzy automatycznie a plików logx.txt x-nr pliku od 1 do 100 plik zawiera nazwę, nazwę skryptu, datę"
+echo "--help / -h wyświetli listę dostępnych opcji"
 }
 while [ "$1" != "" ];
 do
 	case $1 in
-		--date)
+		--date|-d)
 			shift
 			date;;
-		--logs)
+		--logs|-l)
 			z=$2
 			if [ $# -eq 2 ];
 			then
@@ -31,6 +31,9 @@ do
 					echo date>>log$i.txt
 				done
 			fi;;
-		*);;
-		esac
+		--help|-h)
+				shift
+				help;;
+ *);; 
+	esac
 done
