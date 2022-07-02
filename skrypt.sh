a92a920a159cc3cac7f653944d91113c4d079730
Tomasz Dydacki <b4shiken@hotmail.com>
2022-07-02 21:34:56 +0200

    4

--- skrypt.sh
+++ skrypt.sh
@@ -5,13 +5,24 @@ do
 			shift
 			date;;
 		--logs)
-			shift
-			for i in 'seq 1 100'; 
-			do
-				echo "log$i.txt">>log$i.txt
-				echo "skrypt.sh">>log$i.txt
-				echo date>>log$i.txt
-			done;;
+			z=$2
+			if [ $# -eq 2 ];
+			then
+				shift
+				for i in {1..'$z'}; 
+				do
+					echo "log$i.txt">>log$i.txt
+					echo "skrypt.sh">>log$i.txt
+					echo date>>log$i.txt
+				done
+			else
+				for i in `seq 1 100`; do
+					touch log$i.txt
+					echo "log$i.txt">>log$i.txt
+					echo "skrypt.sh">>log$i.txt
+					echo date>>log$i.txt
+				done
+			fi;;
 		*);;
 		esac
 done
